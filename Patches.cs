24a8b3a52763971e54d2c600d1803c9733b7c241
moosemeat817 <michaelsd123456@gmail.com>
2025-04-21 08:12:58 -0500

    Add files via upload

---
+++ Patches.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using Il2Cpp;
+
+namespace StretchArmstrong
+{
+    [HarmonyPatch(typeof(PlayerManager), "ComputeModifiedPickupRange")]
+    internal class PlayerManager_ComputeModifiedPickupRange
+    {
+        private static void Postfix(ref float __result)
+        {
+            __result = __result * Settings.option.modifier;
+        }
+    }
+}
